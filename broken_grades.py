a47675101252e8e4f46c37b18220998fe6265da0
AL <aleenarose3112@gmail.com>
2023-11-24 07:43:56 +0300

    Broken grades added

--- broken_grades.py
+++ broken_grades.py
@@ -19,28 +19,27 @@
 # Grade: F
 # Student iis failing.
 
-exam_one = int(input("Input exam grade one: "))
+exam_one = int(input("Input exam grade one: ")) 
+exam_two = int(input("Input exam grade two: ")) # converted into integer format
 
-exam_two = input("Input exam grade two: "))
+exam_three = int(input("Input exam grade three: ")) #changed the variable exam_3 to exam_three
 
-exam_3 = str(input("Input exam grade three: "))
-
-grades = [exam_one exam_two exam_three]
+grades = [exam_one, exam_two, exam_three]#added commas
 sum = 0
-for grade in grade:
-  sum = sum + grade
+for grade in grades: #changed grade to grades
+    sum = sum + grade
 
-avg = sum / len(grdes)
+avg = sum / len(grades)# changed grdes to grades
 
 if avg >= 90:
     letter_grade = "A"
-elif avg >= 80 and avg < 90
+elif avg >= 80 and avg < 90: #added semicolon
     letter_grade = "B"
 elif avg > 69 and avg < 80:
-    letter_grade = "C'
+    letter_grade = "C" #corrected the mismatched string literals
 elif avg <= 69 and avg >= 65:
     letter_grade = "D"
-elif:
+else: # use else instead of elif
     letter_grade = "F"
 
 for grade in grades:
@@ -50,7 +49,7 @@ for grade in grades:
 
     print("Grade: " + letter_grade)
 
-if letter-grade is "F":
-    print "Student is failing."
+if letter_grade == "F": #changed the hyphen to semicolon to make it defined and is changed to "=="
+    print ("Student is failing.") # added missing parentheses
 else:
-    print "Student is passing."
+    print ("Student is passing.") # added missing parentheses
